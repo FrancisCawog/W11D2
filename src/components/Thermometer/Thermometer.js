5887f72e66ab237b863cd10ece6adb4d6bf87fec
Francis Cawog <Franciscawog@gmail.com>
2023-10-31 15:02:33 -0400

    finished phase 6

--- src/components/Thermometer/Thermometer.js
+++ src/components/Thermometer/Thermometer.js
@@ -1,14 +1,17 @@
 import ReactSlider from "react-slider";
 import './Thermometer.css';
+import { useClimate } from "../../context/ClimateContext";
 
 function Thermometer() {
+  const {temperature, setTemp} = useClimate();
+
   return (
     <section>
       <h2>Thermometer</h2>
-      <div className="actual-temp">Actual Temperature: {"x"}°F</div>
+      <div className="actual-temp">Actual Temperature: {temperature}°F</div>
       <ReactSlider
-        value={40}
-        onAfterChange={(val) => {}}
+        value={temperature}
+        onAfterChange={(val) => {setTemp(val)}}
         className="thermometer-slider"
         thumbClassName="thermometer-thumb"
         trackClassName="thermometer-track"
--- src/context/ClimateContext.js
+++ src/context/ClimateContext.js
@@ -1,2 +1,15 @@
-// Temperature has a default value of 50 degrees
-// Humidity has a default value of 40%
+import { createContext, useState, useContext }from "react";
+
+export const ClimateContext = createContext()
+export const useClimate = () => useContext(ClimateContext);
+
+export default function ClimateProvider(props) {
+    const [temperature, setTemp] = useState(50);
+    const [humidity, setHumidity] = useState(40);
+
+    return (
+        <ClimateContext.Provider value={{temperature, setTemp, humidity, setHumidity}}>
+            {props.children}
+        </ClimateContext.Provider>
+    )
+}
